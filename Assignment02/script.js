c9a4eec538c65d01d7fcd13a9fb5669b25eb0523
Shrutika Gade <126967914+shrutika-07@users.noreply.github.com>
2024-01-17 11:27:20 +0530

    Initial Commit

---
+++ Assignment02/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // Load dynamic content for SDP tab
+    $('#sdp').html('<p>This is dynamically loaded content for SDP.</p>');
+
+    // Load dynamic content for EDI tab
+    $('#edi').html('<p>This is dynamically loaded content for EDI.</p>');
+
+    // Load dynamic content for DT tab
+    $('#dt').html('<p>This is dynamically loaded content for DT.</p>');
+
+    // Load dynamic content for Course Projects tab
+    $('#course').html('<p>This is dynamically loaded content for Course Projects.</p>');
+});
